d5351ac16a03ef92e554bb5b575318f9818ef3f3
WAP246 <kavan_wampler@biswashington.org>
2025-11-24 21:50:30 -0500

    Update base-caller-bookmarklet.js

--- base-caller-bookmarklet.js
+++ base-caller-bookmarklet.js
@@ -1,9 +1,1 @@
-javascript:(function(){
-  const scriptUrl = 'https://raw.githubusercontent.com/WAP246/bookmarklets/main/pranks/gpu-glitch.js';
-
-  const s = document.createElement('script');
-  s.type = 'text/javascript';
-  s.src = scriptUrl;
-
-  document.body.appendChild(s);
-})();
+javascript:(function(){const scriptUrl='https://raw.githubusercontent.com/WAP246/bookmarklets/main/pranks/gpu-glitch.js';const s=document.createElement('script');s.type='text/javascript';s.src=scriptUrl;document.body.appendChild(s);})();
